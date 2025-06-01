51205fb6c59493642894aa0b389c9759fed356c1
Sidnei Henrique <henrisrs@hotmail.com>
2025-06-01 20:48:34 -0300

    #62 - Implementado o paper do JointJS, junto com a funcionalidade de zoom e panning

--- src/app/lucide-icons.module.ts
+++ src/app/lucide-icons.module.ts
@@ -1,30 +1,32 @@
 import { NgModule } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 // Importa os ícones para o componente game-phase
-import { 
-        X, 
-        Store, 
-        Backpack, 
-        CircleDollarSign, 
-        Sparkles, 
-        MoveUpRight,
-        Lightbulb,
-        BookCheck,
-        MessageCircleQuestion,
-        Save } from 'lucide-angular';
+import {
+  X,
+  Store,
+  Backpack,
+  CircleDollarSign,
+  Sparkles,
+  MoveUpRight,
+  Lightbulb,
+  BookCheck,
+  MessageCircleQuestion,
+  Save
+} from 'lucide-angular';
 
 @NgModule({
-  imports: [LucideAngularModule.pick({ 
-                                      X, 
-                                      Store,
-                                      Backpack, 
-                                      CircleDollarSign, 
-                                      Sparkles, 
-                                      MoveUpRight,
-                                      Lightbulb,
-                                      BookCheck,
-                                      MessageCircleQuestion,
-                                      Save })],
+  imports: [LucideAngularModule.pick({
+    X,
+    Store,
+    Backpack,
+    CircleDollarSign,
+    Sparkles,
+    MoveUpRight,
+    Lightbulb,
+    BookCheck,
+    MessageCircleQuestion,
+    Save
+  })],
   exports: [LucideAngularModule],
 })
-export class LucideIconsModule {}
+export class LucideIconsModule { }
