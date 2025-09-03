1e4a8b2beba4e56043541788a4635f88aef6fed4
Sidnei Henrique <henrisrs@hotmail.com>
2025-09-03 19:33:27 -0300

    Mapa do jogo e alterações de rota

---
+++ src/app/game-map/node-activity/node-activity.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+import { LucideIconsModule } from '../../lucide-icons.module';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Game } from '../game-map.component';
+
+export interface Character {
+  name: string;
+  filePath: string;
+}
+
+@Component({
+  selector: 'node-activity',
+  standalone: true,
+  imports: [
+    LucideIconsModule,
+    RouterModule,
+    CommonModule
+],
+  templateUrl: './node-activity.component.html',
+  styleUrl: './node-activity.component.css'
+})
+export class NodeActivityComponent {
+
+  @Input() game!: Game;
+  @Input() last!: boolean;
+
+  constructor(private router: Router) {
+
+  }
+
+  openGamePhase() {
+    this.router.navigate(['/game']);
+  }
+}
